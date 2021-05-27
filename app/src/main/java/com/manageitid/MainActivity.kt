d4df84b793df0a051f82cf8e1ab3b9cb0c7a88f3
Zikrurridho Afwani <zikrurridhoafwani@gmail.com>
2021-05-27 22:54:58 +0800

    Sign in & SIgn Up

--- app/src/main/java/com/manageitid/MainActivity.kt
+++ app/src/main/java/com/manageitid/MainActivity.kt
@@ -7,5 +7,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val DashboardFragment =  Dashboard()
+        val fragment = supportFragmentManager.findFragmentByTag(DashboardFragment::class.java.simpleName)
+
+        if (fragment !is Dashboard) {
+            supportFragmentManager
+                    .beginTransaction()
+                    .add(R.id.fragment_container, DashboardFragment, DashboardFragment::class.java.simpleName)
+                    .commit()
+        }
     }
 }
