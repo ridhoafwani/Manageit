ff176d55956528d9d596dba6b2ee38b73acbd688
Zikrurridho Afwani <zikrurridhoafwani@gmail.com>
2021-05-30 23:02:15 +0800

    Firebase auth

--- app/src/main/java/com/manageitid/Signup.kt
+++ app/src/main/java/com/manageitid/Signup.kt
@@ -1,11 +1,88 @@
 package com.manageitid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.manageitid.databinding.ActivitySignupBinding
 
-class Signup : AppCompatActivity() {
+class Signup : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding : ActivitySignupBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = FirebaseAuth.getInstance()
+
+        binding.btnSignUp.setOnClickListener(this)
+        binding.tvSignIn.setOnClickListener(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            moveToMain()
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when(v){
+            binding.btnSignUp -> register()
+            binding.tvSignIn -> moveToLogin()
+        }
+    }
+
+    fun register(){
+        val name = binding.edtName.text.toString()
+        val email = binding.edtEmail.text.toString()
+        val password = binding.edtPassword.text.toString()
+
+        when{
+            TextUtils.isEmpty(name) -> {
+                binding.edtName.setError("Name cannot empty")
+                binding.edtName.requestFocus()
+            }
+            TextUtils.isEmpty(email) -> {
+                binding.edtEmail.setError("Name cannot empty")
+                binding.edtEmail.requestFocus()
+            }
+            TextUtils.isEmpty(password) -> {
+                binding.edtPassword.setError("Name cannot empty")
+                binding.edtPassword.requestFocus()
+            }
+            else -> {
+                auth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(baseContext, "Registration Succsesfully",
+                                Toast.LENGTH_SHORT).show()
+                            val user = auth.currentUser
+                            moveToLogin()
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(baseContext, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }
+        }
+
+    }
+
+    fun moveToLogin(){
+        startActivity(Intent(baseContext, Login::class.java))
+    }
+
+    fun moveToMain(){
+        startActivity(Intent(baseContext, MainActivity::class.java))
     }
 }
